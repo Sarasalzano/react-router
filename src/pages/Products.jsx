efb0ff7d06b0e0d4de01f158fa681f762a67b0d0
Sara Salzano <sarasalzano95@gmail.com>
2025-10-06 15:13:23 +0200

    esegui product card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,34 @@
-export default function Products(){
-    
+import { useState, useEffect } from "react";
+
+export default function CardProducts(){
+   //setto variabile di stato dei prodotti come array vuoto
+    const [products, setProducts] =useState([]);
+    //chiamata axios per recuperare i prodotti da inserire nelle card
+    useEffect(()=>{
+            axios.get("https://fakestoreapi.com/products")
+            .then(res => setProducts(res.data))
+            .catch(err => console.log(err));
+        }, []);
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="column">
+                    <div className="card">
+                        {products.map((product) => (
+                        <>                        
+                        <div key={product.id} className="img-wrapper">
+                            <img src={product.image} alt={product.title} />
+                        </div>
+                        <div className="item-title">{product.title}</div>
+                        <div className="item-price">{product.price}</div>
+                        <div className="item-description">{product.description}</div>
+                        <div className="item-rate">{product.rating.rate} ({product.rating.count} reviews)</div>
+                        </>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
