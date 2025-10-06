9bbcd46de177f9f41da3fb45acb06f0c66ab958a
Sara Salzano <sarasalzano95@gmail.com>
2025-10-06 14:32:50 +0200

    route nesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from "./pages/Homepage";
 import './App.css';
+import Homepage from "./pages/Homepage";
+import Layout from './components/Layout';
+import About from './pages/About';
+import Products from './pages/Products';
 
 function App() {
 
@@ -9,7 +12,12 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />}> </Route>
+        {/* Route nesting: il genitore Layout raggiunge le route figlie */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="products" element={<Products />}/>
+        </Route>
       </Routes>
       </BrowserRouter>
     </>
