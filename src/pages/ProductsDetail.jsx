c13b534745d3edd0c5a66bee79bd040ee89ab5b1
Sara Salzano <sarasalzano95@gmail.com>
2025-10-07 14:54:09 +0200

    chiamata axios

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -1,10 +1,20 @@
 //import useprarams per leggere parametri dinamici dall'url
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ProductsDetail(){
 //recupero id
 const {id} =useParams();
 
+// stato dell'oggetto ricavato da api
+const [product, setProduct] =useState({});
+
+//chiamata api per recuperare oggetto corrispondende a id dinamico
+useEffect(()=>{
+    axios.get(`https://fakestoreapi.com/products/${id}`)
+    .then(res => setProduct(res.data))
+    .catch(err => console.log(err))
+},[])
 
     return(
         <h2>id prodotto: {id}</h2>
